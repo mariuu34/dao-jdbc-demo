288360e96f13cc021d2bade0b069df85d2a77362
Mario Mendes <mariuu.34@gmail.com>
2020-02-13 12:07:23 -0200

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Deparment;
+import model.entities.Seller;
 
 public class Program {
-	
-	public static void main(String [] args) {
-		Deparment obj = new Deparment(1,"Books");
-		System.out.println(obj);
+
+	public static void main(String[] args) {
+		Deparment obj = new Deparment(1, "Books");
+
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 
 }
